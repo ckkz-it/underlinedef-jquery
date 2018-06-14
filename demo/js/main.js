70767f84631e7feeb9d95590ec745240505cb039
ckkz-it <cirkus.kz@gmail.com>
2018-06-14 17:33:03 +0300

    small fixes v1.0.1

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,16 +1,13 @@
-function ready() {
+$(function() {
 
 	var words = ["aquatic", "krill", "temperate"],
-			definitions = ["The term water bird, waterbird or aquatic bird is used to refer to birds that live on or around water.", "Krill are small crustaceans of the order Euphausiacea, and are found in all the world's oceans.", "In geography, the temperate or tepid climates of Earth occur in the middle latitudes, which span between the tropics and the polar regions of Earth."];
+		definitions = ["The term water bird, waterbird or aquatic bird is used to refer to birds that live on or around water.", "Krill are small crustaceans of the order Euphausiacea, and are found in all the world's oceans.", "In geography, the temperate or tepid climates of Earth occur in the middle latitudes, which span between the tropics and the polar regions of Earth."];
 
-
-	underlineDef(".text", {
+	$(".text").underlineDef({
 		words: words,
 		definitions: definitions,
 		search: 'google',
 		// underlineClass: 'customUnderline'
 	});
 
-}
-
-document.addEventListener('DOMContentLoaded', ready);
+});
